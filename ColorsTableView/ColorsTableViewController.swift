c9443991ba821180f1d251ab76ca42f918a0b450
Max Taylor <mjttk5@mail.missouri.edu>
2018-03-23 22:13:32 -0500

    New Features

--- ColorsTableView/ColorsTableViewController.swift
+++ ColorsTableView/ColorsTableViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ColorsTableViewController: UIViewController {
+class ColorsTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let colors: [String] = ["red", "orange", "green", "blue", "purple", "brown"]
+    
+    let cellReuseIdentifier = "cell"
+    
+    @IBOutlet weak var tableVIew: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Register the table view cell class and its reuse id
+        self.tableVIew.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        // (optional) include this line if you want to remove the extra empty cell divider lines
+        // self.tableView.tableFooterView = UIView()
+        
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        tableVIew.delegate = self
+        tableVIew.dataSource = self
     }
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = self.tableVIew.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.colors[indexPath.row]
+        
+        return cell
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
